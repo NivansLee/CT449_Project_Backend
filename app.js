d56e49a4637ed2e8a5715d0bb6adf6a682b4f621
NivansLee <khoaledang2003.stvn@gmail.com>
2024-11-17 18:07:17 +0700

    Định nghĩa các route cho tài nguyên staff

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");// Nhập thư viện Express.js để tạo ứng dụng web backend.
+const cors = require("cors"); // Nhập thư viện CORS (Cross-Origin Resource Sharing) để hỗ trợ các yêu cầu từ nguồn khác.
+const app = express(); // Tạo một ứng dụng Express.
 
-// Tạo một ứng dụng Express mới
-const app = express();
+const staffRouter = require("./app/route/staff.route")
 
-// Sử dụng middleware cors để cho phép các yêu cầu từ các nguồn khác
-app.use(cors());
+app.use(cors());// Sử dụng middleware cors để cho phép các yêu cầu từ các nguồn khác
+app.use(express.json()); // Sử dụng middleware express.json() để parse dữ liệu JSON từ các request
 
-// Sử dụng middleware express.json() để parse dữ liệu JSON từ các request
-app.use(express.json());
+app.use("/api/staff", staffRouter);
 
 // Định nghĩa một route GET cho đường dẫn gốc ('/')
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     // Gửi một phản hồi JSON chứa một message
-    res.json({ message: 'Welcome to book management application.' });
+    res.json({ message: "Welcome to book management application." });
 });
 
 // Xuất ứng dụng Express để có thể sử dụng ở các file khác
